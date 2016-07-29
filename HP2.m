9d9be092b1914eb29d24a3f625176d2f6db185f6
Meili Feng <meili.feng0116@gmail.com>
2016-07-29 16:53:08 +0200

    Update HP2.m

--- HP2.m
+++ HP2.m
@@ -1,3 +1,13 @@
+% This code is written by Meili Feng (meili.feng0116@gmail.com), 29 July 2016
+
+% the function is for calculation of hydropeaking characterization indicator hp2 - rate of change
+% apply methodology developed by Carolli et al. 2015
+% check the original method description and applications as below paper information:
+% paper citation: Carolli, M. Davide V, Annunziato S, Guido Z, Maria C.B, Knut A. ...
+% A simple procedure for the assessment of hydropeaking flow alterations applied to several European streams. ...
+% Aquatic Sciences 77, 639-653, doi:10.1007/s00027-015-0408-5 (2015).
+
+
 function hp2 = HP2( vector,DT )
 %TP Summary of this function goes here
 %   DT is the time interval to be calculated 
