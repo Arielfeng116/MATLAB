a78347bb84b032037dbbf599a9c6445782b5a367
Meili Feng <meili.feng0116@gmail.com>
2016-07-29 17:11:33 +0200

    Update findT3.m

--- findT3.m
+++ findT3.m
@@ -1,5 +1,14 @@
+% this function is written by Meili Feng (meili.feng0116@gmail.com)
+% All rights reserved. 
+% 
+% the function offers extraction of subset data by year and day e.g. 1980-30, or 1980 only
+
+
 function T = findT3(rawdata,year,day)
 %% finds year and day
+% input rawdata format: year-month-day (1-3colums) and data (4:end columns)
+% output T dataset: rows of corresponding date and all columns 
+
 switch nargin
     case 2
         v = (rawdata (:,1)==year);
